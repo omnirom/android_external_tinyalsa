62d292f4ba4410c57efe922e5f9c8bc02c81f7bc
Shiv Maliyappanahalli <smaliyap@codeaurora.org>
2014-07-20 22:45:52 +0700

    pcm: add support for pcm_ioctl() api
    Tinyalsa doesn't expose an api to call ioctls exposed by
    the audio drivers. Add pcm_ioctl() api.
    
    Change-Id: Ie519840671f858c23b0acea7155b365c0d9ca59a
    (cherry picked from commit bf0ff7a28a8c85508c0bbd1de46e085632fa3579)
    (cherry picked from commit 72deed2f35003610748f5d61863348219ddbdd85)
    (cherry picked from commit 3a0912fa06d3d1383b9ef84914c8eb864fe11136)

--- pcm.c
+++ pcm.c
@@ -1039,3 +1039,15 @@ int pcm_mmap_read(struct pcm *pcm, void *data, unsigned int count)
 
     return pcm_mmap_transfer(pcm, data, count);
 }
+
+int pcm_ioctl(struct pcm *pcm, int request, ...)
+{
+    va_list ap;
+    void * arg;
+
+    va_start(ap, request);
+    arg = va_arg(ap, void *);
+    va_end(ap);
+
+    return ioctl(pcm->fd, request, arg);
+}
